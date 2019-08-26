09f85e95ef97704b40807d0f1533ed3ec7bbf23c
nickryanweber <nicholasryanweber@gmail.com>
2019-08-26 18:26:07 -0400

    added materialize styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,49 @@ class Home extends Component {
   render() {
     return (
       <>
-        <h1>Home</h1>
+        <article class="row">
+          <section class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">Linkin Park</span>
+                <p>
+                  My friends hate all my Linkin Park jokes, but in the end it
+                  doesn't even matter.
+                </p>
+              </div>
+              <div class="card-action">
+                <Link to="/linkin-park">Check out Linkin Park</Link>
+              </div>
+            </div>
+          </section>
+          <section class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">System of a Down</span>
+                <p>It's a non-stop disco / Bet you it's Nabisco</p>
+              </div>
+              <div class="card-action">
+                <Link to="/system-of-a-down">Check out System of a Down</Link>
+              </div>
+            </div>
+          </section>
+          <section class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">Green Day</span>
+                <p>
+                  If Green Day were the Beatles, instead of Abbey Road what
+                  would they have instead? The Boulevard of Broken Dreams.
+                </p>
+              </div>
+              <div class="card-action">
+                <Link to="/green-day">Check out Green Day</Link>
+              </div>
+            </div>
+          </section>
+        </article>
+
+        {/* <h1>Home</h1>
         <h2>Linkin Park</h2>
         <p>
           My friends hate all my Linkin Park jokes, but in the end it doesn't
@@ -26,7 +68,7 @@ class Home extends Component {
         </p>
         <Link to="/green-day">
           <button>Check it out</button>
-        </Link>
+        </Link> */}
       </>
     )
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,41 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Navbar, NavItem } from 'react-materialize'
 
 export class NavBar extends Component {
   render() {
     return (
-      <nav>
-        <h1>test</h1>
-        <ul>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/linkin-park">
-            <li>Linkin Park</li>
-          </Link>
-          <Link to="/system-of-a-down">
-            <li>System of a Down</li>
-          </Link>
-          <Link to="/green-day">
-            <li>Green Day</li>
-          </Link>
-        </ul>
-      </nav>
+      <>
+        <Navbar brand={<Link to="/">Home</Link>} alignLinks="right">
+          <NavItem>
+            <Link to="/linkin-park">Linkin Park</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/system-of-a-down">System of a Down</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/green-day">Green Day</Link>
+          </NavItem>
+        </Navbar>
+
+        {/* <nav>
+          <h1>test</h1>
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/linkin-park">
+              <li>Linkin Park</li>
+            </Link>
+            <Link to="/system-of-a-down">
+              <li>System of a Down</li>
+            </Link>
+            <Link to="/green-day">
+              <li>Green Day</li>
+            </Link>
+          </ul>
+        </nav> */}
+      </>
     )
   }
 }
