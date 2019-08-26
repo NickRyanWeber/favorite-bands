aca00c089e056d11757fa1b4caf974e8cb00c716
nickryanweber <nicholasryanweber@gmail.com>
2019-08-26 15:45:26 -0400

    cleaned up unused files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Home from './components/Home.jsx'
-import LinkinPark from './components/LinkinPark.jsx'
-import SystemOfADown from './components/SystemOfADown.jsx'
-import GreenDay from './components/GreenDay.jsx'
 import Band from './components/Band.jsx'
 
 class App extends Component {
@@ -30,13 +27,6 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/:band" component={Band}></Route>
-            {/* <Route exact path="/linkin-park" component={LinkinPark}></Route>
-            <Route
-              exact
-              path="/system-of-a-down"
-              component={SystemOfADown}
-            ></Route>
-            <Route exact path="/green-day" component={GreenDay}></Route> */}
           </Switch>
         </Router>
       </>
